795f8d63c438ddd40663c43fe8e373ed36392578
imagefilterbotV1 <96852831+imagefilterbotV1@users.noreply.github.com>
2022-01-20 19:36:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('♻️ OUR GROUP ♻️', url='https://t.me/joinchat/BhYlk3vvhG5hMmZl')
+                InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
             ],
             [
                 InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
